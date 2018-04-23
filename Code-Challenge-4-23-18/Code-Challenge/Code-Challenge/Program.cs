f4d2f0f36eec69feaf671817f7fc00ce07629c93
smithnicholas94 <38564680+smithnicholas94@users.noreply.github.com>
2018-04-23 11:45:11 -0400

    Add files via upload
    Coding Challenge

---
+++ Code-Challenge-4-23-18/Code-Challenge/Code-Challenge/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge
+{
+    class Program
+    {
+        /*
+        Strings to test:
+        racecar
+        Racecar
+        1221
+        never Odd, or Even.
+        1231      
+        aBc 
+        */
+
+        //create an array of chars
+        char[] charArray;
+
+        public string Reverse(string s)
+        {
+            //reverse the characters in the array
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static void Main(string[] args)
+        {
+            //create method to see if a method is a Palindrome or not
+
+            //add Strings to a string array
+            string[] stringArray = new string[6];
+
+            stringArray[0] = "racecar";
+            stringArray[1] = "Racecar";
+            stringArray[2] = "1221";
+            stringArray[3] = "never Odd, or Even.";
+            stringArray[4] = "1231";
+            stringArray[5] = "aBc";
+
+            //Print the reverse of the string each string.
+
+            //Compare the newly reversed string with the original string.
+
+            //If the strings match, return True.
+
+            //If the strings do NOT match, return False. 
+
+          
+            //test print
+            Console.WriteLine("racecar");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
